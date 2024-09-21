2e7407e1763adab5e941d40fead091f31b251f57
gabulyan_t_qschool <tigran.gabulyan.2001@mail.ru>
2024-09-21 21:59:54 +0300

    feat: adding service-workers

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,6 @@ import { BrowserRouter } from "react-router-dom";
 // routes
 import { MainRoutes } from "./routes/MainRoutes";
 
-// serviceWorkers
-import { registerServiceWorker } from "@serviceWorkers/registerServiceWorker";
-
 //mantine
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
@@ -18,8 +15,6 @@ import { ModalsProvider } from "@mantine/modals";
 // styles
 import "@mantine/core/styles.css";
 import "@app/index.css";
-
-registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
